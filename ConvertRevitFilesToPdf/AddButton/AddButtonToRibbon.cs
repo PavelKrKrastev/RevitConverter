dfea4f2f12f58e4423db8a7c9fd13b446074e9f2
PavelKrKrastev <pkrastev19@gmail.com>
2017-09-19 18:22:20 +0300

    Add button to ribbon, convert logic not implemented

---
+++ ConvertRevitFilesToPdf/AddButton/AddButtonToRibbon.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.UI;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace ConvertRevitFilesToPdf
+{
+    public class AddButtonToRibbon : IExternalApplication
+    {
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            AddRibbonPanel(application);
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        private void AddRibbonPanel(UIControlledApplication application)
+        {
+            string tabName = "Converter";
+            application.CreateRibbonTab(tabName);
+
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Convert to PDF");
+
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData buttonData = new PushButtonData("cmdSaveDocumentAsPdf",
+               "Convert to PDF", thisAssemblyPath, "ConvertRevitFilesToPdf.SaveDocumentAsPdf");
+
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+            pushButton.ToolTip = "Convert current document to pdf";
+
+            pushButton.LargeImage = RibbonButtonImageSource("ConvertRevitFilesToPdf.Resources.convert.png");
+        }
+
+        #region Get image for ribbon button from resources
+        private System.Windows.Media.ImageSource RibbonButtonImageSource(string EmbeddedPath)
+        {
+            Stream ImageSream = this.GetType().Assembly.GetManifestResourceStream(EmbeddedPath);
+            var PngDecoder = new PngBitmapDecoder(ImageSream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            return PngDecoder.Frames[0];
+        }
+        #endregion Get image for ribbon button from resources
+    }
+}
---
+++ ConvertRevitFilesToPdf/Converter/SaveDocumentAsPdf.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using Autodesk.Revit.DB;
+using System.Windows.Forms;
+using System.IO;
+using Autodesk.Revit.Attributes;
+
+namespace ConvertRevitFilesToPdf
+{
+    [Transaction(TransactionMode.Manual)]
+    class SaveDocumentAsPdf : IExternalCommand
+    {
+        #region Constants
+        private const string _saveDialogFilter = "PDF|*.pdf";
+        private const string _defaultPdfNameFormat = " PDF";
+        #endregion Constants
+
+        #region Private Properties
+        private string _saveAsPdfPath { get; set; }
+        private SaveFileDialog _saveDialog = new SaveFileDialog() { Filter = _saveDialogFilter };
+        #endregion Private Properties
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                Document document = commandData.Application.ActiveUIDocument.Document;
+                string name = document.Title;
+                _saveDialog.FileName = Path.GetFileNameWithoutExtension(name);
+
+                if (_saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // TO DO Implement convert logic (rvt->pdf)
+                }
+
+                return Result.Succeeded;
+            }
+            // This is where we "catch" potential errors and define how to deal with them
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                // If user decided to cancel the operation return Result.Canceled
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                // If something went wrong return Result.Failed
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
